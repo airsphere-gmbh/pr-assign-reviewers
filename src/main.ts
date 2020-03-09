cb3d1600589514fabe591fc1f1402cf18c34f02c
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2020-03-09 18:05:45 +0100

    Write an action to add all reviewers as assignee
    * Write an action and try to get alle reviewer as assignee to ensure that later all person avatars are shown up

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { GitHub as GitHubClient } from "@actions/github/lib/github";
+import GitHub from "@actions/github";
+import Core from "@actions/core";
+
+function run() {
+  const context = GitHub.context;
+  const token = Core.getInput("repo-token");
+  const client = new GitHubClient(token);
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  let number: number; 
+
+if(context.payload.pull_request)
+    number = context.payload.pull_request.number;
+else if(context.payload.issue)
+    number = context.payload.issue.number;
+else
+    number = -1;
+
+  client.pulls.listReviewRequests({
+      owner,
+      repo,
+      number
+  }).then(l => {
+      let assignees = l.data.users.map(u => u.login);
+      client.issues.addAssignees(
+          {
+            owner,
+            repo,
+            issue_number: number,
+            assignees
+          }
+      );
+  })
+}
+
+//Entrypoint for github
+run();
